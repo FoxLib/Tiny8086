c5bdb1a8d702e11d881cc813cde085c292bc6886
FoxLib <hplus.foxtail@gmail.com>
2021-05-05 09:31:33 +0300

    Переделал АЛУ

--- 07_ja88core/alu.v
+++ 07_ja88core/alu.v
@@ -20,15 +20,13 @@ wire parity  = ~^res[7:0];
 wire zerof   = isize ? ~|res : ~|res[7:0];
 wire signf   = res[isize ? 15 : 7];
 wire carryf  = res[isize ? 16 : 8];
+wire auxf    = op1[4]^op2[4]^res[4];
 
 // Самая хитрая логика из всего тут
-wire add8_o  = !((op1[ 7] ^ op2[ 7]) & (op1[ 7] ^ res[ 7]));
-wire add16_o = !((op1[15] ^ op2[15]) & (op1[15] ^ res[15]));
-
-wire [4:0] adda = op1[3:0] + op2[3:0];
-wire [4:0] adca = op1[3:0] + op2[3:0] + flags[0];
-wire [4:0] suba = op1[3:0] - op2[3:0];
-wire [4:0] sbba = op1[3:0] - op2[3:0] - flags[0];
+wire add8o   = (op1[ 7] ^ op2[ 7] ^ 1) & (op1[ 7] ^ res[ 7]);
+wire sub8o   = (op1[ 7] ^ op2[ 7] ^ 0) & (op1[ 7] ^ res[ 7]);
+wire add16o  = (op1[15] ^ op2[15] ^ 1) & (op1[15] ^ res[15]);
+wire sub16o  = (op1[15] ^ op2[15] ^ 0) & (op1[15] ^ res[15]);
 
 always @* begin
 
@@ -50,14 +48,14 @@ always @* begin
         // ADD | ADC
         0, 2: flags_o = {
 
-            /* O */ (isize ? add16_o : add8_o),
+            /* O */ (isize ? add16o : add8o),
             /* D */ flags[10],
             /* I */ flags[9],
             /* T */ flags[8],
             /* S */ signf,
             /* Z */ zerof,
             /* 0 */ 1'b0,
-            /* A */ (alumode == 2 ? adca[4] : adda[4]),
+            /* A */ auxf,
             /* 0 */ 1'b0,
             /* P */ parity,
             /* 1 */ 1'b1,
@@ -67,14 +65,14 @@ always @* begin
         // SBB | SUB | CMP
         3, 5, 7: flags_o = {
 
-            /* O */ ~(isize ? add16_o : add8_o),
+            /* O */ (isize ? sub16o : sub8o),
             /* D */ flags[10],
             /* I */ flags[9],
             /* T */ flags[8],
             /* S */ signf,
             /* Z */ zerof,
             /* 0 */ 1'b0,
-            /* A */ (alumode == 3 ? sbba[4] : suba[4]),
+            /* A */ auxf,
             /* 0 */ 1'b0,
             /* P */ parity,
             /* 1 */ 1'b1,
