b1f71f6d9944a7a9a3b410c0a60df0039b98050a
FoxLib <kotolisic@gmail.com>
2021-05-09 08:00:16 +0300

    Fix bios

--- 07_ja88core/bios/bios.asm
+++ 07_ja88core/bios/bios.asm
@@ -1,16 +1,14 @@
 
             org     0
 
-            jmp     near m1
-            hlt
             mov     ax, $b800
             mov     ds, ax
             mov     ax, $1721
 m1:         mov     cx, 2000
             xor     bx, bx
 @@:         mov     [bx], ax
-            add     al, 1
+            sub     al, 1
             add     bx, 2
             dec     cx
             jne     @b
-            je      m1
+            jmp     $
